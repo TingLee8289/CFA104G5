9ba7cc99e95d08e62b8cd2b66f0b47a81f52d24c
Ting Lee <happiness8289@yahoo.com.tw>
2022-02-16 16:04:33 +0800

    test

--- src/main/java/util/TestJedis測試Jedis.java
+++ src/main/java/util/TestJedis測試Jedis.java
@@ -6,7 +6,6 @@ public class TestJedis測試Jedis {
 	public static void main(String[] args) {
 		Jedis jedis = new Jedis("localhost", 6379);
 		System.out.println(jedis.ping());
-		
 		jedis.close();
 	}
 }
