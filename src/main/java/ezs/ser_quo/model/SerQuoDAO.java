7c2370454763048d5aaaf5434abdb662ddc97e20
Jimmy Liu <jimmypc03@gmail.com>
2022-02-16 17:29:56 +0800

    新增廠商檢視需求單

--- src/main/java/ezs/ser_quo/model/SerQuoDAO.java
+++ src/main/java/ezs/ser_quo/model/SerQuoDAO.java
@@ -21,7 +21,7 @@ public class SerQuoDAO implements SerQuoDAO_interface {
 	private static final String GET_ONE_STMT = "SELECT QUO_ID,QUO_STATUS,QUO_DMD_ID,QUO_VDR_ID,QUO_DATE,QUO_EXPIRYDATE,QUO_ITEM, QUO_TOTALPRICE FROM `ser_quo` where QUO_ID = ?";
 	private static final String DELETE = "DELETE FROM `ser_quo` where QUO_ID = ?";
 	private static final String UPDATE = "UPDATE `ser_quo` set QUO_STATUS=?, QUO_DMD_ID=?, QUO_VDR_ID=?, QUO_DATE=?, QUO_EXPIRYDATE=?, QUO_ITEM=?, QUO_TOTALPRICE=? where QUO_ID = ?";
-
+	public static final String FIND_BY_QUO_VDRID = "SELECT * FROM ser_quo WHERE quo_vdr_id = ?";
 	// 一個應用程式中,針對一個資料庫 ,共用一個DataSource即可
 			private static DataSource ds = null;
 			static {
@@ -140,6 +140,37 @@ public class SerQuoDAO implements SerQuoDAO_interface {
 		try {
 			con = ds.getConnection();
 			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				// 每次進來迴圈裡，就代表一筆資料，我們就產生一個Bean，包裝著查詢的資料，最後再回傳給呼叫端
+				serQuoVO = new SerQuoVO();
+				
+				serQuoVO.setQuoID(rs.getInt("QUO_ID"));
+				serQuoVO.setQuoStatus(rs.getByte("QUO_STATUS"));
+				serQuoVO.setQuoDmdID(rs.getInt("QUO_DMD_ID"));
+				serQuoVO.setQuoVdrID(rs.getInt("QUO_VDR_ID"));
+				serQuoVO.setQuoDate(rs.getDate("QUO_DATE"));
+				serQuoVO.setQuoExpiryDate(rs.getDate("QUO_EXPIRYDATE"));
+				serQuoVO.setQuoItem(rs.getString("QUO_ITEM"));
+				serQuoVO.setQuoTotalPrice(rs.getBigDecimal("QUO_TOTALPRICE"));
+				list.add(serQuoVO);
+			}
+			
+			
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}finally {
+			Util.closeResource(con, pstmt, rs);
+		}
+		return list;
+	}
+	public List<SerQuoVO> findByVdrID(Integer quoVdrID) {
+		List<SerQuoVO> list = new ArrayList<SerQuoVO>();
+		SerQuoVO serQuoVO = null;
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(FIND_BY_QUO_VDRID);
+			pstmt.setInt(1, quoVdrID);
 			rs = pstmt.executeQuery();
 			while(rs.next()) {
 				// 每次進來迴圈裡，就代表一筆資料，我們就產生一個Bean，包裝著查詢的資料，最後再回傳給呼叫端
