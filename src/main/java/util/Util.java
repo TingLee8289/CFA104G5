2c4f0ca4299dc90a15e272d88e3f75f0700ac6c3
Luke <chuanyun0108@gmgil.com>
2022-02-16 12:46:49 +0800

    修改util帳密

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class Util {
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	public static final String URL = "jdbc:mysql://localhost:3306/CFA104G5?serverTimezone=Asia/Taipei";
-	public static final String USER = "root";
-	public static final String PASSWORD = "password";
+	public static final String USER = "CFA104G5";
+	public static final String PASSWORD = "CFA104G5";
 
 	public static void closeResource(Connection con, PreparedStatement pstmt, ResultSet rs) {
 		if (rs != null) {
