32bade299f0380e6067862a3330e5d9e461c5ccc
Doris <rr965234@gmail.com>
2022-02-23 11:20:57 +0800

    password 0223 am

--- src/main/java/ezs/member/model/ResetPasswordMailService.java
+++ src/main/java/ezs/member/model/ResetPasswordMailService.java
@@ -28,8 +28,8 @@ public class ResetPasswordMailService {
 
 				// ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
 				// ●須將myGmail的【安全性較低的應用程式存取權】打開
-				final String myGmail = "rr965234@gmail.com";
-				final String myGmail_password = "OOPP098123";
+				final String myGmail = "ixlogic.wu@gmail.com";
+				final String myGmail_password = "BBB45678BBB";
 				Session session = Session.getInstance(props, new Authenticator() {
 					protected PasswordAuthentication getPasswordAuthentication() {
 						return new PasswordAuthentication(myGmail, myGmail_password);
